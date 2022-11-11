7c261d6e67c50e9f3c4a7d6a36ece07908e18a8e
yamashitatubasa <k021g1247@g.neec.ac.jp>
2022-11-11 15:11:09 +0900

    02_02.マテリアル

--- Object3d.h
+++ Object3d.h
@@ -29,11 +29,40 @@ public: // サブクラス
 		XMFLOAT2 uv;  // uv座標
 	};
 
-	// 定数バッファ用データ構造体
-	struct ConstBufferData
+	// 定数バッファ用データ構造体B0
+	struct ConstBufferDataB0
 	{
-		XMFLOAT4 color;	// 色 (RGBA)
+		//XMFLOAT4 color;	// 色 (RGBA)
 		XMMATRIX mat;	// ３Ｄ変換行列
+	};
+
+	// 定数バッファ用データ構造体B1
+	struct ConstBufferDataB1
+	{
+		XMFLOAT3 ambient; // アンビエント係数
+		float pad1;       // パディング
+		XMFLOAT3 diffuse; // ディフューズ係数
+		float pad2;       // パディング
+		XMFLOAT3 specular; // スペキュラー係数
+		float alpha;       // アルファ
+	};
+
+	// マテリアル
+	struct Material
+	{
+		std::string name; // マテリアル名
+		XMFLOAT3 ambient; // アンビエント影響度
+		XMFLOAT3 diffuse; // ディフェーズ影響度
+		XMFLOAT3 specular;// スペキュラー影響度
+		float alpha;      // アルファ
+		std::string textureFilename; // テクスチャファイル名
+		// コンストラクタ
+		Material() {
+			ambient = { 0.3f,0.3f,0.3f };
+			diffuse = { 0.0f,0.0f,0.0f };
+			specular = { 0.0f,0.0f,0.0f };
+			alpha = 1.0f;
+		}
 	};
 
 private: // 定数
@@ -142,6 +171,8 @@ private: // 静的メンバ変数
 	// 頂点インデックス配列
 	//static unsigned short indices[planeCount * 3];
 	static std::vector<unsigned short> indices;
+	// マテリアル
+	static Material material;
 
 private:// 静的メンバ関数
 	/// <summary>
@@ -165,7 +196,7 @@ private:// 静的メンバ関数
 	/// <summary>
 	/// テクスチャ読み込み
 	/// </summary>
-	static void LoadTexture();
+	static void LoadTexture(const std::string& directoryPath, const std::string& filename);
 
 	/// <summary>
 	/// モデル作成
@@ -176,6 +207,12 @@ private:// 静的メンバ関数
 	/// ビュー行列を更新
 	/// </summary>
 	static void UpdateViewMatrix();
+
+	/// <summary>
+	/// マテリアル読み込み
+	/// </summary>
+	/// <returns>成否</returns>
+	static void LoadMaterial(const std::string& directoryPath, const std::string& filename);
 
 public: // メンバ関数
 	bool Initialize();
@@ -202,7 +239,9 @@ public: // メンバ関数
 	void SetPosition(const XMFLOAT3& position) { this->position = position; }
 
 private: // メンバ変数
-	ComPtr<ID3D12Resource> constBuff; // 定数バッファ
+	//ComPtr<ID3D12Resource> constBuff; // 定数バッファ
+	ComPtr<ID3D12Resource> constBuffB0; // 定数バッファ
+	ComPtr<ID3D12Resource> constBuffB1; // 定数バッファ
 	// 色
 	XMFLOAT4 color = { 1,1,1,1 };
 	// ローカルスケール
