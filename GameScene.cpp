94f57041423ac3631bf21bbd8b9df0c3a2998690
yamashitatubasa <k021g1247@g.neec.ac.jp>
2023-02-10 14:17:36 +0900

    球と三角形

--- GameScene.cpp
+++ GameScene.cpp
@@ -56,6 +56,12 @@ void GameScene::Initialize(DirectXCommon* dxCommon, Input* input)
 	plane.normal = XMVectorSet(0, 0.5f, 0, 0); // 法線ベクトル
 	plane.distance = 0.0f; // 原点(0,0,0)からの距離
 
+	// 三角形の初期値を設定
+	triangle.p0 = XMVectorSet(-1.0f, 0, -1.0f, 1); // 左手前
+	triangle.p1 = XMVectorSet(-1.0f, 0, +1.0f, 1); // 左奥
+	triangle.p2 = XMVectorSet(+1.0f, 0, -1.0f, 1); // 左手前
+	triangle.normal = XMVectorSet(0.0f, 1.0f, 0.0f, 0); // 上向き
+
 	// 3Dオブジェクト生成
 	object3d = Object3d::Create();
 	object3d->Update();
@@ -171,6 +177,22 @@ void GameScene::Update()
 	if (isHitColor == true) { modelSphere->SetColor(XMFLOAT3(1, 0, 0)); }
 	else { modelSphere->SetColor(XMFLOAT3(1, 1, 1)); }
 
+	// 球と三角形の当たり判定
+	XMVECTOR inter;
+	bool hit = Collision::CheckSphere2Triangle(sphere, triangle, &inter);
+	if (hit) {
+		debugText.Print("HIT", 50, 200, 1.0f);
+		// stringstreamをリセットし、交点座標を埋め込む
+		spherestr.str("");
+		spherestr.clear();
+		spherestr << "("
+			<< std::fixed << std::setprecision(2)
+			<< inter.m128_f32[0] << ","
+			<< inter.m128_f32[1] << ","
+			<< inter.m128_f32[2] << ")";
+
+		debugText.Print(spherestr.str(), 50, 220, 1.0f);
+	}
 }
 
 void GameScene::Draw()
--- GameScene.h
+++ GameScene.h
@@ -83,6 +83,8 @@ private: // メンバ変数
 	Sphere sphere;
 	// 当たり判定 平面
 	Plane plane;
+	// 当たり判定 三角形
+	Triangle triangle;
 
 	bool isHitColor = false; // 当たったときのカラーフラグ
 	bool isMove = false; // 移動フラグ
